98cc45ce6df5eb40afc1eb27e6c431e35955a238
Azimbek Sagynbaev <sagynbaev6@gmail.com>
2022-06-14 04:47:13 +0300

    Send function has been added

---
+++ MyDataCoin/Helpers/StaticFunctions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace MyDataCoin.Helpers
+{
+	public static class StaticFunctions
+	{
+        public static bool IsValidEmail(string email)
+        {
+            var trimmedEmail = email.Trim();
+
+            if (trimmedEmail.EndsWith("."))
+            {
+                return false; // suggested by @TK-421
+            }
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == trimmedEmail;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
+
---
+++ MyDataCoin/Models/SendModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyDataCoin.Models
+{
+	public class SendModel
+	{
+		[Required(ErrorMessage = "User Id cannot be null")]
+		public Guid userid { get; set; }
+
+		[Required(ErrorMessage = "Address cannot be null")]
+		public string Address { get; set; }
+
+		[Required(ErrorMessage = "Cannot be null")]
+		public double Amount { get; set; }
+	}
+}
+
